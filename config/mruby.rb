21148226f8894fb9e039aa5e94b444eb5df6d610
Mavenlink Provisioning User <provision@kickseed.hsd1.ca.comcast.net>
2015-09-05 16:13:35 -0700

    fix ruby conf

--- config/mruby.rb
+++ config/mruby.rb
@@ -19,25 +19,3 @@ MRuby::Build.new do |conf|
   # conf.gembox 'default'
   conf.bins = ["mrbc"]
 end
-
-MRuby::Build.new('host-debug') do |conf|
-  # load specific toolchain settings
-
-  # Gets set by the VS command prompts.
-  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
-    toolchain :visualcpp
-  else
-    toolchain :gcc
-  end
-
-  enable_debug
-
-  # include the default GEMs
-  conf.gembox 'default'
-
-  # C compiler settings
-  conf.cc.defines = %w(ENABLE_DEBUG)
-
-  # Generate mruby debugger command (require mruby-eval)
-  conf.gem :core => "mruby-bin-debugger"
-end
