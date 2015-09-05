d6e99ac4bba034047f4e93051bd39cf803539776
Mavenlink Provisioning User <provision@kickseed.hsd1.ca.comcast.net>
2015-09-05 15:19:06 -0700

    add debian ppa

---
+++ debian/detective-agency.default.ex
@@ -0,0 +1,10 @@
+# Defaults for detective-agency initscript
+# sourced by /etc/init.d/detective-agency
+# installed at /etc/default/detective-agency by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
