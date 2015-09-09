b5355839d84e7b5bb80c4816f4845b703416a348
mavenlink <jon@mavenlink.com>
2015-09-08 23:19:33 -0700

    revert to dev version

--- Makefile
+++ Makefile
@@ -1,70 +1,20 @@
-# OSX Makefile
+# Makefile
 
 product=detective-agency
-build=build
+build=/tmp/$(product)-build
 target=$(build)/$(product)
-fpm_tmp:=$(shell mktemp -d -u -t fpm_XXXXX)
 
-CXXFLAGS=\
--Wall \
--g \
--pg \
--Imruby/include \
--Ilibyaml/include
+objects = $(patsubst %,$(build)/%, $(patsubst %.c,%.o, $(wildcard *.c)))
 
-CFLAGS=$(CXXFLAGS)
-LDFLAGS=
+$(target): $(build) $(objects)
+	$(CC) $(LDFLAGS) -o $@ $(objects)
 
-objects = $(patsubst %,build/%, $(patsubst %.c,%.o, $(wildcard *.c)))
-ruby_headers = $(patsubst %,build/%, $(patsubst lib/%.rb,%.h, $(wildcard lib/*.rb)))
-
-$(target): $(build) $(objects) yaml/src/.libs/libyaml.a mruby/build/host/lib/libmruby.a $(ruby_headers)
-	$(CC) $(LDFLAGS) -o $@ $(objects) mruby/build/host/lib/libmruby.a yaml/src/.libs/libyaml.a -lm
-
-test: $(build)/test.yml
-	ansible-playbook -vvv -i inventory $(build)/test.yml
-
-$(build)/test.yml: $(target) Detectivefile
-	$(target) > $@
-
-mruby/bin/mrbc: mruby/build/host/lib/libmruby.a
-
-mruby/build/host/lib/libmruby.a:
-	cd mruby && MRUBY_CONFIG=../config/mruby.rb make
-
-yaml/src/.libs/libyaml.a:
-	cd yaml && ./configure CFLAGS="-DYAML_DECLARE_STATIC" --enable-static --disable-shared && make
-
-$(build)/%.o: %.c $(ruby_headers)
-	$(CC) $(CXXFLAGS) -c $< -o $@
-
-$(build)/%.h: lib/%.rb mruby/bin/mrbc
-	mruby/bin/mrbc -g -B $(patsubst build/%.h,%, $@) -o $@ $<
+clean:
+	touch $(build) && rm -R $(build)
 
 $(build):
 	mkdir -p $(build)
 
-yaml/Makefile:
-	cd yaml && ./bootstrap && ./configure
+$(build)/%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
 
-install: $(target)
-	mkdir -p $(DESTDIR)/usr/bin
-	install $(target) $(DESTDIR)/usr/bin/$(product)
-
-package:
-	make install DESTDIR=$(fpm_tmp)
-	fpm -s dir -t deb -n detective-agency -v 0.1.0 -C $(fpm_tmp) -d ansible usr/bin
-
-ppa:
-#make clean
-# remove .git dirs
-#dh_make --createorig -c gpl -e diclophis+ubuntu@gmail.com -s -a -p detective-agency_0.2.0
-#debuild -S -sa
-#cd ..
-#dput -f -u ppa:diclophis/detective-agency detective-agency_0.2.0-1_source.changes 
-	echo read Makefile for manual instructions
-
-clean: yaml/Makefile
-	cd yaml && make clean
-	cd mruby && make clean
-	touch $(build) && rm -R $(build)
--- main.c
+++ main.c
@@ -1,95 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <mruby.h>
-#include <mruby/array.h>
-#include <mruby/irep.h>
-#include <mruby/compile.h>
-#include <mruby/string.h>
-#include <mruby/error.h>
-
-#include "build/init.h"
-
-static void if_exception_error_and_exit(mrb_state* mrb, char *msg) {
-  // check for exception
-  if (mrb->exc)
-  {
-    // print msg and exception
-    fprintf(stderr, "%s", msg);
-    mrb_print_error(mrb);
-    exit(3);
-  }
-}
-
-
-static mrb_value business(mrb_state* mrb, mrb_value obj)
-{
-  mrb_value ret;
-  mrb_value block;
-  mrb_get_args(mrb, "&", &block);
-
-  ret = mrb_yield_argv(mrb, block, 0, NULL);
-
-  if_exception_error_and_exit(mrb, "Exception in business yield\n");
-
-  char *tasks = mrb_str_to_cstr(mrb, ret);
-  fprintf(stdout, "%s", tasks);
-
-  return mrb_nil_value();
-}
-
-
 int main(int argc, char** argv) {
-  mrb_state *mrb;
-  mrb_value ret;
-
-  // initialize mruby
-  if (!(mrb = mrb_open()))
-  {
-    fprintf(stderr,"%s: could not initialize mruby\n",argv[0]);
-    return -1;
-  }
-
-  mrb_value args = mrb_ary_new(mrb);
-  int i;
-
-  // convert argv into mruby strings
-  for (i=1; i<argc; i++)
-  {
-     mrb_ary_push(mrb, args, mrb_str_new_cstr(mrb,argv[i]));
-  }
-
-  mrb_define_global_const(mrb, "ARGV", args);
-
-  mrb_define_method(mrb, mrb->object_class, "business", business, MRB_ARGS_BLOCK());
-
-  int bundled_ruby_count = 1;
-  uint8_t const **bundled_ruby = (uint8_t const **)malloc(sizeof(const uint8_t *) * bundled_ruby_count);
-  bundled_ruby[0] = init;
-  for (i=0; i<bundled_ruby_count; i++) {
-    // load the compiled library
-    ret = mrb_load_irep(mrb, bundled_ruby[i]);
-    if_exception_error_and_exit(mrb, "Exception in bundled ruby\n");
-  }
-
   FILE *f = 0;
   f = fopen("Detectivefile", "r");
   if (0 == f) {
-    return 2;
+    return 1;
   }
 
-  //no-debug mode
-  //ret = mrb_load_file(mrb, f);
-
-  mrbc_context *cc = mrbc_context_new(mrb);
-  mrbc_filename(mrb, cc, "Detectivefile");
-  ret = mrb_load_file_cxt(mrb, f, cc);
-  mrbc_context_free(mrb, cc);
   fclose(f);
-  if_exception_error_and_exit(mrb, "Exception in Detectivefile\n");
-
-  // cleanup
-  mrb_close(mrb);
 
   return 0;
 }
